30dedaa32558bf3def6cb6f0cd56f73bf18d784a
oksanababak1 <oksana.babak@oa.edu.ua>
2025-12-01 23:06:26 +0200

    Lab 4 completed: Vue 3 + TypeScript + Vue Router

---
+++ src/views/ContactView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import UserCard from '../components/UserCard.vue';
+</script>
+
+<template>
+  <div class="contact-page">
+    <h1>Сторінка контактів</h1>
+    <p>Це наша нова сторінка контактів!</p>
+
+    <UserCard username="Student_1" :isActive="true" />
+    <UserCard username="Student_2" :isActive="false" />
+    <!--Помилка TypeScript при розкоментуванні:-->
+    <!--Type 'number' is not assignable to type 'boolean'-->
+   <!--<UserCard username="Student_3" :isActive="123" />-->
+  </div>
+</template>
+
+<style scoped>
+.contact-page {
+  padding: 20px;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// Логіка для головної сторінки
+</script>
+
+<template>
+  <div class="home">
+    <h1>Вітаємо у Vue 3!</h1>
+    <p>Це головна сторінка вашого застосунку</p>
+  </div>
+</template>
+
+<style scoped>
+.home {
+  padding: 20px;
+  text-align: center;
+}
+
+h1 {
+  color: #42b983;
+}
+</style>
